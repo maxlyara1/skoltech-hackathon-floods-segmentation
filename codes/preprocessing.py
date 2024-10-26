71ec37e4dfb5b631e7a3702d7080d9509f24cbda
maxlyara1 <maks.lyara@gmail.com>
2024-10-26 20:56:45 +0300

    first commit

---
+++ codes/preprocessing.py
@@ -0,0 +1,3 @@
+
+
+def get_images_from_folder()
---
+++ run_calculation.sh
@@ -0,0 +1,8 @@
+python calculate_metrics.py \
+    --osm_path='/Users/mir/Desktop/dataset/train/osm/9.geojson' \
+    --preds_path='/Users/mir/Desktop/dataset/train/example_preds' \
+    --masks_path='/Users/mir/Desktop/dataset/train/masks' \
+    --pre_gt_path='/Users/mir/Desktop/dataset/train/masks/9_1.tif' \
+    --post_gt_path='/Users/mir/Desktop/dataset/train/masks/9_2.tif' \
+    --pre_pred_path='/Users/mir/Desktop/dataset/train/example_preds/9_1.tif' \
+    --post_pred_path='/Users/mir/Desktop/dataset/train/example_preds/9_2.tif'
